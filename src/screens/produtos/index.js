2a7ae6e8febc62941ebd8c2066a3f140347783a3
patricklouback <patrickmlouback@gmail.com>
2022-07-27 22:04:27 -0300

    criando FlatList para produtos

--- src/screens/produtos/index.js
+++ src/screens/produtos/index.js
@@ -1,15 +1,59 @@
 import React, {useState, useEffect} from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity, Modal, Pressable } from 'react-native';
+import { View, Text, Image, FlatList, TouchableOpacity, Modal, Pressable } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 import { AntDesign  } from '@expo/vector-icons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getFirestore, setDoc, doc,} from 'firebase/firestore';
+import { getFirestore, getDoc , doc,} from 'firebase/firestore';
 import styles from './styles';
 
 export default function Produtos() {
     const route = useRoute();
     const {uid} = route.params;
+
+    const produtosList = [
+        {
+          id: '1',
+          name: "Produto 1",
+          valor: '5.90',
+          description:
+            "Teste descrição",
+        },
+        {
+            id: '2',
+            name: "Produto 2",
+            valor: '10.50',
+            description:
+            "Teste descrição 2",
+        },
+        {
+            id: '3',
+            name: "Produto 1",
+            valor: '5.90',
+            description:
+              "Teste descrição",
+          },
+          {
+              id: '4',
+              name: "Produto 2",
+              valor: '10.50',
+              description:
+              "Teste descrição 2",
+          },
+          {
+            id: '5',
+            name: "Produto 1",
+            valor: '5.90',
+            description:
+              "Teste descrição",
+          },
+          {
+              id: '6',
+              name: "Produto 2",
+              valor: '10.50',
+              description:
+              "Teste descrição 2",
+          }
+    ];
 
     function abreTelaHome () {
         navigation.navigate('home', {uid: uid})
@@ -30,6 +74,22 @@ export default function Produtos() {
                 source= { require('../../images/logo.png') }
             />
         </View>
+
+        <FlatList 
+                    data={produtosList}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({item}) => 
+                        <View style={styles.containerFlat}>
+                            <View style={styles.contentFlat}>
+                                <Text style={styles.titleFlat}>{item.name}</Text>
+                                <Text style={styles.descriptionFlat}>{item.valor}</Text>
+                                <Text numberOfLines={5} style={styles.descriptionFlat}>{item.description}</Text>
+                            </View>
+                        </View>
+                     }
+        />
+
+        <View style = { styles.viewInferior }></View>
     </View>
     );
   }
--- src/screens/produtos/styles.js
+++ src/screens/produtos/styles.js
@@ -20,6 +20,36 @@ const styles = StyleSheet.create({
       top: 80,
       left: 30,
     },
+    scrowView:{
+      width: '100%',
+      backgroundColor: '#000'
+    },
+    viewInferior:{
+      width: '100%',
+      height: 50,
+    },
+    containerFlat: {
+      width: '100%',
+      backgroundColor: "#0202",
+      alignItems: "center",
+      justifyContent: "space-between",
+      paddingHorizontal: 16,
+      paddingVertical: 20,
+    },
+    contentFlat: {
+      flex: 1,
+      marginLeft: 16,
+    },
+    titleFlat: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: "#6F4E37",
+      marginBottom: 16,
+    },
+    descriptionFlat: {
+      fontSize: 16,
+      color: "#000",
+    },
   });
-
+//style = { styles.scrowView }
   export default styles;
